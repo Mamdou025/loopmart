ee5634117eb584db154ba4ec958d2f7f3b3d1dc6
Mamadou Fall <fallmamadou151@gmail.com>
2022-05-13 14:53:42 -0400

    Modal animation data toggle

--- src/Components/Carte.js
+++ src/Components/Carte.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Panier } from "../App"
-import { Modal } from "./Modal"
+import { Modal,ModalZ } from "./Modal"
 import { Options } from "./Commentaires"
 import { Description } from "./Commentaires"
 import { Commentaires } from "./Commentaires"
@@ -24,7 +24,7 @@ export class Seller extends React.Component{
         <div>
         <div className="seller">
         <div className="options">
-       <button type="button" className="btn btn-light plus" onClick={this.props.showModal}><i className="bi bi-arrows-angle-expand"id="expand"></i></button>
+       <button type="button" className="btn btn-light plus" data-toggle="modal" data-target={"#"+`${this.props.auteur+this.props.montant}`}><i className="bi bi-arrows-angle-expand"id="expand"></i></button>
        <button type="button" className="btn btn-light plus" ><i className="bi bi-bookmark"></i></button>
       <button type="button" className="btn btn-light plus" onClick={this.props.Addproduct}><i className="bi bi-cart-plus"></i></button>
   
@@ -72,59 +72,11 @@ export class Seller extends React.Component{
         <a href={this.props.src}><img className="rectangle" src={this.props.src} /></a>
   
   
-      <Seller Addproduct={this.props.Addproduct} showModal={this.showModal} show={this.state.show} src={this.props.src} montant={this.props.montant} auteur={this.props.auteur} profil={this.props.profil} description={this.props.description}/>
+      <Seller Addproduct={this.props.Addproduct} src={this.props.src} montant={this.props.montant} auteur={this.props.auteur} profil={this.props.profil} description={this.props.description}/>
       
-      <Modal hideModal={this.hideModal} show={this.state.show}  src={this.props.src}  montant={this.props.montant} auteur={this.props.auteur} profil={this.props.profil} description={this.props.description}/>
+      <ModalZ  src={this.props.src}  montant={this.props.montant} auteur={this.props.auteur} profil={this.props.profil} description={this.props.description}/>
   
-      <div>
-<button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
-  Launch demo modal
-</button>
-
-<div className="modal fade" id="exampleModalCenter" tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div className="modal-dialog modal-dialog-centered" role="document">
-
-    <div>
-    <div className="modalrow1">
-    <div className="modalcell">
-    <div>
-      <div className="seller">
-      <div className="options">
-     <button type="button" className="btn btn-light plus" onClick={this.props.hideModal}><i className="bi bi-arrows-angle-contract"></i></button>
-
-    </div>
-        
-      <div className="sellerinfo">
-          <span  className="auteur">{this.props.auteur}</span> 
-        <img className="profil" src={this.props.profil} />
-      </div>
-    </div>
-  
-    </div>
-
-    </div>
-  </div>
-  <div className="modalrow">
- 
-    <div className="modalcell" id="i7ai">
-  
-    <a href={this.props.src}><img className="rectangle" src={this.props.src} /></a>
-    </div>
-    <div className="modalcell2">
-      <Options/>
       
-      <Description src={this.props.src}  montant={this.props.montant + '$'} auteur={this.props.auteur} profil={this.props.profil} description={this.props.description}/>
-
-     
-      <Commentaires/>
-
-    </div>
-  </div>
-    </div>
-
-  </div>
-</div>
-      </div>
       </div>
       )
     }
--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -63,3 +63,63 @@ export class Modal extends React.Component{
   )
   }
 }
+
+
+export class ModalZ extends React.Component{
+
+  //{(this.props.show)?"modal showit":"modal"}
+  
+    render(){
+    return (
+      
+
+
+      <div className="modal fade" id={`${this.props.auteur+this.props.montant}`} tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div className="modal-dialog modal-dialog-centered" role="document">
+      
+          <div>
+          <div className="modalrow1">
+          <div className="modalcell">
+          <div>
+            <div className="seller">
+            <div className="options">
+           <button type="button" className="btn btn-light plus"data-dismiss="modal" aria-label="Close"><i className="bi bi-arrows-angle-contract"></i></button>
+      
+          </div>
+              
+            <div className="sellerinfo">
+                <span  className="auteur">{this.props.auteur}</span> 
+              <img className="profil" src={this.props.profil} />
+            </div>
+          </div>
+        
+          </div>
+      
+          </div>
+        </div>
+        <div className="modalrow">
+       
+          <div className="modalcell" id="i7ai">
+        
+          <a href={this.props.src}><img className="rectangle" src={this.props.src} /></a>
+          </div>
+          <div className="modalcell2">
+            <Options/>
+            
+            <Description src={this.props.src}  montant={this.props.montant + '$'} auteur={this.props.auteur} profil={this.props.profil} description={this.props.description}/>
+      
+           
+            <Commentaires/>
+      
+          </div>
+        </div>
+          </div>
+      
+        </div>
+      </div>
+            
+     
+    )
+    }
+  }
+  
